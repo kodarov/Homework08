c7ac487ee3c60326c6f08e229d35ca5a8c1c5979
kodarov_salavat <kodarov@gmail.com>
2023-01-21 12:57:11 +0400

    Hw08\task02

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
+        task1task2();
         task3();
         task4();
     }
-    public static void task1(){
-        System.out.println("Задание 1");
+    public static void task1task2(){
+        System.out.println("Задание 1, Задание 2");
         int firstArray[] = new int[] {1,2,3};
         double secondArray[] = {1.57, 7.654, 9.986};
-        String strintArray[] = {"one","","two"};
-    }
-    public static void task2(){
-        System.out.println("Задание 2");
+        String stringArray[] = {"one","two","three"};
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i]+" ");
+        }
+        System.out.println();
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i]+ " ");
+        }
+        System.out.println();
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.print(stringArray[i]+ " ");
+        }
+        System.out.println();
     }
     public static void task3(){
         System.out.println("Задание 3");
