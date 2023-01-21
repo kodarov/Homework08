3c6cc3c4e0d4ceaf37ad133ad8e9efb4f2e7fa18
kodarov_salavat <kodarov@gmail.com>
2023-01-21 16:28:42 +0400

    Hw08\task03

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-        task1task2();
-        task3();
-        task4();
+        task1_2_3_4();
     }
-    public static void task1task2(){
-        System.out.println("Задание 1, Задание 2");
+    public static void task1_2_3_4(){
+        System.out.println("Задание 1");
         int firstArray[] = new int[] {1,2,3};
         double secondArray[] = {1.57, 7.654, 9.986};
         String stringArray[] = {"one","two","three"};
+
+        System.out.println("Задание 2");
         for (int i = 0; i < firstArray.length; i++) {
-            System.out.print(firstArray[i]+" ");
+            System.out.print(firstArray[i]);
+            if (i != firstArray.length-1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         for (int i = 0; i < secondArray.length; i++) {
-            System.out.print(secondArray[i]+ " ");
+            System.out.print(secondArray[i]);
+            if (i != secondArray.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         for (int i = 0; i < stringArray.length; i++) {
-            System.out.print(stringArray[i]+ " ");
+            System.out.print(stringArray[i]);
+            if (i != stringArray.length-1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-    }
-    public static void task3(){
+
         System.out.println("Задание 3");
-    }
-    public static void task4(){
-        System.out.println("Задание 4");
+        for (int i = firstArray.length-1; i >= 0 ; i--) {
+            System.out.print(firstArray[i]);
+            if (i != 0) {
+            System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = secondArray.length-1; i >= 0 ; i--) {
+            System.out.print(secondArray[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = stringArray.length-1; i >= 0 ; i--) {
+            System.out.print(stringArray[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
     }
 }
